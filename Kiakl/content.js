0138aac00ebeb9d79f174a1b0de7dcbcf974f404
Hakase <davidsommerfield.zg@gmail.com>
2023-12-20 09:31:37 -0600

    restructed code to capture every relevant key and minimize error

--- Kiakl/content.js
+++ Kiakl/content.js
@@ -1,85 +1,135 @@
 let started = false;
-let lastStrokeTime = 0;
 let typedTime = 0;
 let history = []
-let i = -1;
+let t = -1;
+
+
+const isCorrect = (key, duration) => {
+    // const activeWordElement = document.querySelector('#words .word.active');
+    const letters = activeWordElement.querySelectorAll('letter');
+    let correct = false;
+
+    if (key.length == 1) {
+        if (key.match(/[a-zA-Z0-9]/)) {
+            for (let i = letters.length - 1; i >= 0; i--) {
+                const letter = letters[i];
+
+                if (letter.classList.length > 0) {
+                    correct = letter.classList.contains('correct');
+                    break;
+                }
+            }
+        } else if (key == " ") {
+            correct = (letters[letters.length - 1].classList.length > 0)
+        }
+    }
+
+    console.log(`${duration} ${timeToType}: ${getKeyAlias(key)} ${correct ? '✓' : 'x'}`);
+    t += 1;
+}
+
+const trackMonkeytype = (key, duration) => {
+    const timer = document.querySelector('#typingTest .time');
+
+    if (timer.style.opacity > 0) {
+        if (!started) {
+            started = true;
+            duration = 0;
+        }
+
+        requestAnimationFrame(() => {
+            isCorrect(key, duration); // Pass currentKey to isCorrect function
+        });
+    } else {
+        requestAnimationFrame(() => {
+            trackMonkeytype(key, duration); // Pass currentKey to isCorrect function
+        });
+    }
+}
+
+const getKeyAlias = (key) => {
+    switch (key) {
+        case "Enter":
+            return "↵";
+        case "Backspace":
+            return "⌫"
+    }
+
+    return key;
+}
+
+let currentKey = "";
+let lastStrokeTime = 0;
+let timeToType = 0;
 
 window.onkeydown = function (event) {
+    // If the key is a space then we need to see if the second to last word is full yet
+    let now = performance.now()
+    timeToType = now - lastStrokeTime;
+    lastStrokeTime = now;
+
     const currentUrl = window.location.href;
 
     if (currentUrl === "https://monkeytype.com/") {
-        const activeWordElement = document.querySelector('#words .word.active');
-        const timer = document.querySelector('#typingTest .time');
+        trackMonkeytype(event.key, timeToType);
 
-        if (timer.style.opacity > 0) {
+        /*if (timer.style.opacity > 0) {
             if (!started) {
                 started = true;
                 lastStrokeTime = performance.now();
-                console.log("started");
+                console.log("started", event.key);
             }
+            console.log(event.key);
 
-            if (activeWordElement) {
-                const letters = activeWordElement.querySelectorAll('letter');
-                let char = " ";
-                let isCorrect = null;
+            // console.log(`${i} ${typedTime.toFixed(1)}: ${char} ${inputted_key} ${isCorrect ? '✓' : 'x'}`);
 
-                for (let i = letters.length - 1; i >= 0; i--) {
-                    const letter = letters[i];
-                    if (letter.classList.length > 0) {
-                        isCorrect = letter.classList.contains('correct');
-                        char = letter.textContent;
-                        break;
-                    }
-                }
+            // keypress = {
+            //     key: inputted_key,
+            //     correct: isCorrect,
+            //     duration: typedTime,
+            // };
 
-                console.log(`${i} ${typedTime.toFixed(1)}: ${char} ${event.key} ${isCorrect ? '✓' : 'x'}`);
+            // inputted_key = event.key;
 
-                keypress = {
-                    key: char,
-                    correct: isCorrect,
-                    duration: typedTime,
-                };
+            // typedTime = performance.now() - lastStrokeTime;
+            // lastStrokeTime = performance.now();
 
-                typedTime = performance.now() - lastStrokeTime;
-                lastStrokeTime = performance.now();
+            // history.push(keypress)
+        }*/
+    } /*else if (started) {
+        started = false;
+        console.log("ended");
 
-                history.push(keypress)
-            }
-        } else if (started) {
-            started = false;
-            console.log("ended");
+        const session = {
+            website: currentUrl,
+            sessionID: Date.now(),
+            data: history,
+        };
 
-            const session = {
-                website: currentUrl,
-                sessionID: Date.now(),
-                data: history,
-            };
+        // Retrieve the 'log' array from local storage and update it with the new session
+        chrome.storage.local.get({ log: [] }, (result) => {
+            const log = result.log;
+            log.push(session);
 
-            // Retrieve the 'log' array from local storage and update it with the new session
-            chrome.storage.local.get({ log: [] }, (result) => {
-                const log = result.log;
-                log.push(session);
-
-                log.forEach((session, index) => {
-                    console.log(`Session ${index + 1}:`);
-                    console.log('Website:', session.website);
-                    console.log('Session ID:', session.sessionID);
-                    console.log('Data:', session.data);
-                    console.log('-------------------------');
-                });
-
-                // Store the updated 'log' array back into local storage
-                chrome.storage.local.set({ log }, () => {
-                    if (chrome.runtime.lastError) {
-                        console.error('Error while saving session:', chrome.runtime.lastError);
-                    } else {
-                        console.log('Session saved successfully!');
-                    }
-                });
+            log.forEach((session, index) => {
+                console.log(`Session ${index + 1}:`);
+                console.log('Website:', session.website);
+                console.log('Session ID:', session.sessionID);
+                console.log('Data:', session.data);
+                console.log('-------------------------');
             });
-        }
 
-    } else if (!currentUrl.includes("login")) {
+            // Store the updated 'log' array back into local storage
+            chrome.storage.local.set({ log }, () => {
+                if (chrome.runtime.lastError) {
+                    console.error('Error while saving session:', chrome.runtime.lastError);
+                } else {
+                    console.log('Session saved successfully!');
+                }
+            });
+        });
+    }*/
+
+} /*else if (!currentUrl.includes("login")) {
         console.log(event.key + Date.now().toString());
-    }
-};
+    } */
